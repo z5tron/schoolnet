514c88501514d5d1be8ac6412bf4e7fd6dcd4be0
Alex <z5tron@github.com>
2024-10-02 22:57:13 -0400

    add menu

--- schoolnet.php
+++ schoolnet.php
@@ -39,14 +39,23 @@ class SCN_Course {
 
     public static function init() {
         register_post_type(
-            'course',
+            'scn_course',
             array(
                 'labels' => array(
-                    'name' => __( 'Course' ),
-                    'singular_name' => __( 'Course' )
+                    'name' => __( 'Courses' ),
+                    'singular_name' => __( 'Course' ),
+                    'add_new' => __( 'Add New' ),
+                    'add_new_item' => __( 'Add New Course' ),
+                    'edit_item' => __( 'Edit Course' ),
+                    'new_item' => __( 'New Course' ),
+                    'view_item' => __( 'View Course' ),
+                    'search_item' => __( 'Search Course' ),
                 ),
                 'public' => true,
-                'has_archive' => true,
+                'has_archive' => false,
+                'show_in_menu' => false,
+                'menu_icon' => 'dashicons-book-alt',
+                'supports' => array( 'title', 'comments', 'teacher', 'thumbnail', 'custom-fields' ),
             )
         );
     }
@@ -54,4 +63,60 @@ class SCN_Course {
 
 add_action( 'init', array('SCN_Course', 'init') );
 
+
+class SCN_Transaction {
+    function __construct( $post_id = NULL ) {
+        if ( !empty($post_id)) $this->getPost( $post_id );
+    }
+
+    function getPost( $post_id ) {
+        $this->post = get_post( $post_id );
+
+        // set properties for easy access
+        if ( !empty( $this->post ) ) {
+            $this->id = $this->post->ID;
+            // ...
+        }
+        if ( !empty( $this->id ) ) return $this->id;
+        return false;
+    }
+
+    public static function init() {
+        register_post_type(
+            'scn_transaction',
+            array(
+                'labels' => array(
+                    'name' => __( 'Transaction' ),
+                    'singular_name' => __( 'Transaction' ),
+                    'add_new' => __( 'Add New' ),
+                    'add_new_item' => __( 'Add New Transaction' ),
+                    'edit_item' => __( 'Edit Transaction' ),
+                    'new_item' => __( 'New Transaction' ),
+                    'view_item' => __( 'View Transaction' ),
+                    'search_item' => __( 'Search Transaction' ),
+                ),
+                'public' => true,
+                'has_archive' => false,
+                'show_in_menu' => false,
+                'menu_icon' => 'dashicons-book-alt',
+                'supports' => array( 'title', 'comments', 'teacher', 'thumbnail', 'custom-fields' ),
+            )
+        );
+    }
+}
+
+add_action( 'init', array('SCN_Transaction', 'init') );
+
+
+add_action( 'admin_menu', 'scn_main_menu' );
+function scn_main_page() {
+    echo '<h1 class="wp-heading-inline">interesting main page!!</h1>';
+}
+function scn_main_menu() {
+    add_menu_page( 'SchoolNet Plugin Page',
+        'SchoolNet', 'manage_options', 'schoolnet-plugin', 'scn_main_page', 'dashicons-welcome-learn-more', 10);
+    add_submenu_page('schoolnet-plugin', 'Courses', 'Courses', 'manage_options', 'edit.php?post_type=scn_course');
+
+    add_submenu_page('schoolnet-plugin', 'Transactions', 'Transactions', 'manage_options', 'edit.php?post_type=scn_transaction');
+}
 ?>
