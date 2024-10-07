7a00c1a83b61645bfdcedf79b6c1b41f082087d8
Alex <z5tron@github.com>
2024-10-06 20:37:31 -0400

    update

---
+++ class-scn-course.php
@@ -0,0 +1,230 @@
+<?php
+
+/** class */
+
+class SCN_Course {
+    function __construct( $post_id = NULL ) {
+        if ( !empty($post_id)) $this->getPost( $post_id );
+    }
+
+    function getPost( $post_id ) {
+        $this->post = get_post( $post_id );
+
+        // set properties for easy access
+        if ( !empty( $this->post ) ) {
+            $this->id = $this->post->ID;
+            // ...
+        }
+        if ( !empty( $this->id ) ) return $this->id;
+        return false;
+    }
+
+    public static function init() {
+        register_post_type(
+            'scn_course',
+            array(
+                'labels' => array(
+                    'name' => __( 'Courses' ),
+                    'singular_name' => __( 'Course' ),
+                    'add_new' => __( 'Add New' ),
+                    'add_new_item' => __( 'Add New Course' ),
+                    'edit_item' => __( 'Edit Course' ),
+                    'new_item' => __( 'New Course' ),
+                    'view_item' => __( 'View Course' ),
+                    'search_item' => __( 'Search Course' ),
+                ),
+                'public' => true,
+                'has_archive' => false,
+                'show_in_menu' => false,
+                'menu_icon' => 'dashicons-book-alt',
+                'supports' => array( 'title', 'comments', 'thumbnail' ),
+            )
+        );
+
+        /* register school year */
+        /*    
+	    register_taxonomy(
+		    'scn_course_school_year',
+		    'scn_course',
+		    array(
+			    'labels' => array(
+				    'name' => 'School Year',
+				    'add_new_item' => 'Add New School Year',
+				    'new_item_name' => 'New School Year (YYYY-YYYY)',
+			),
+			'show_ui' => true,
+			'meta_box_cb' => false,
+			'show_tagcloud' => false,
+			'hierarchical' => true,
+		)*/
+    }
+}
+
+add_action( 'init', array('SCN_Course', 'init') );
+
+/*
+// Register function to be called when admin interface is visited
+// add_action( 'admin_init', 'scn_admin_init' );
+
+// Function to register new meta box for book review post editor
+function scn_admin_init() {
+	add_meta_box( 'scn_course_details_meta_box', 'Course Details', 'scn_display_course_details_mb', 'scn_course', 'normal', 'high' );
+}
+*/
+
+// Function to display meta box contents
+function scn_display_course_details_mb( $scn_course ) { 
+	// Retrieve current author and rating based on book review ID
+	$teacher = get_post_meta( $scn_course->ID, 'teacher', true );
+	$capacity = get_post_meta( $scn_course->ID, 'capacity', true );
+    $tuition = get_post_meta( $scn_course->ID, 'tuition', true);
+    $school_year = get_post_meta( $scn_course->ID, 'school_year', true);
+
+    $all_teachers = array( 'Master A', 'Master B', 'Dr C' );
+	?>
+	<table>
+        <tr>
+			<td style="width: 150px">School Year</td>
+			<td><input type="text" style="width:100%" name="scn_course_school_year" value="<?php echo intval( $school_year ); ?>"/></td>
+		</tr>
+		<tr>
+			<td style="width: 150px">Capacity</td>
+			<td><input type="number" style="width:100%" name="scn_course_capacity" value="<?php echo intval( $capacity ); ?>" /></td>
+		</tr>
+		<tr>
+			<td style="width: 150px">Teacher</td>
+			<td>
+				<select style="width: 130px" name="scn_course_teacher">
+					<option value="">Select teacher</option>
+					<!-- Loop to generate all items in dropdown list -->
+					<?php foreach ( $all_teachers as $t) { ?>
+					<option value="<?php echo $t; ?>" <?php echo selected( $t, $teacher ); ?>><?php echo $t; ?>
+					<?php } ?>
+				</select>
+			</td>
+		</tr>
+        <tr>
+        <td style="width: 150px">Tuition</td>
+        <td><input type="text" style="width:100%" name="scn_course_tuition" value="<?php echo floatval( $tuition ); ?>" /></td>
+        </tr>
+	</table>
+
+<?php }
+
+// Register function to be called when posts are saved
+// The function will receive 2 arguments
+add_action( 'save_post', 'scn_add_course_fields', 10, 2 );
+
+function scn_add_course_fields( $course_id, $course ) {
+    error_log(print_r($course) );
+	if ( 'scn_course' != $course->post_type ) {
+		return;
+	}
+
+	if ( isset( $_POST['scn_course_school_year'] ) ) {
+		update_post_meta( $course_id, 'school_year', sanitize_text_field( $_POST['scn_course_school_year'] ) );
+	}
+	if ( isset( $_POST['scn_course_teacher'] ) ) {
+		update_post_meta( $course_id, 'teacher', sanitize_text_field( $_POST['scn_course_teacher'] ) );
+	}
+	if ( isset( $_POST['scn_course_capacity'] ) && !empty( $_POST['scn_course_capacity'] ) ) {
+		update_post_meta( $course_id, 'capacity', intval( $_POST['scn_course_capacity'] ) );
+	}
+    if ( isset( $_POST['scn_course_tuition'] ) && !empty( $_POST['scn_course_tuition'] ) ) {
+		update_post_meta( $course_id, 'tuition', floatval( $_POST['scn_course_tuition'] ) );
+	}
+
+	/*******************************************************************
+	* Code from recipe 'Hiding the taxonomy editor from the post editor 
+	* while remaining in the admin menu'
+	*******************************************************************/
+/*
+	if ( isset( $_POST['book_review_book_type'] ) ) {
+		wp_set_post_terms( $book_review->ID, intval( $_POST['book_review_book_type'] ), 'book_reviews_book_type' );
+	}
+*/
+}
+
+/****************************************************************************
+ * Code from recipe 'Displaying additional columns in custom post list page'
+ ****************************************************************************/
+
+// Register function to be called when column list is being prepared
+add_filter( 'manage_edit-scn_course_columns', 'scn_course_add_columns' );
+
+// Function to add columns for author and type in book review listing
+// and remove comments columns
+function scn_course_add_columns( $columns ) {
+    $new_columns = array();
+    $new_columns['scn_course_school_year'] = 'School Year';
+    $new_columns['scn_course_teacher'] = 'Teacher';
+    $new_columns['scn_course_capacity'] = 'Capacity';
+    $new_columns['scn_course_tuition'] = 'Tuition';
+
+    unset( $columns['comments'] );
+    $columns = array_slice( $columns, 0, 2 ) + $new_columns + array_slice( $columns, 2 );
+    
+    return $columns;
+}
+
+// Register function to be called when custom post columns are rendered
+add_action( 'manage_posts_custom_column', 'scn_course_populate_columns' );
+
+// Function to send data for custom columns when displaying items
+function scn_course_populate_columns( $column ) {
+	if ( 'scn_course' != get_post_type() ) {
+		return;
+    }
+
+	// Check column name and send back appropriate data
+	if ( 'scn_course_school_year' == $column ) {
+        echo esc_html( get_post_meta( get_the_ID(), 'school_year', true ) );
+    }
+    elseif ( 'scn_course_teacher' == $column ) {
+        echo esc_html( get_post_meta( get_the_ID(), 'teacher', true ) );
+    }
+    elseif ( 'scn_course_capacity' == $column ) {
+		echo intval( get_post_meta( get_the_ID(), 'capacity', true ) );
+	}
+	elseif ( 'scn_course_tuition' == $column ) {
+		echo '$' . floatval( get_post_meta( get_the_ID(), 'tuition', true ) );
+	}
+}
+
+add_filter( 'manage_edit-scn_course_sortable_columns', 'scn_course_column_sortable' );
+
+// Register the author and rating columns are sortable columns
+function scn_course_column_sortable( $columns ) {
+	$columns['scn_course_school_year'] = 'scn_course_school_year';
+	$columns['scn_course_teacher'] = 'scn_course_teacher';
+	$columns['scn_course_capacity'] = 'scn_course_capacity';
+
+	return $columns;
+}
+
+// Register function to be called when queries are being prepared to
+// display post listing
+add_filter( 'request', 'scn_course_column_ordering' );
+
+// Function to add elements to query variable based on incoming arguments
+function scn_course_column_ordering( $vars ) {
+	if ( !is_admin() ) {
+		return $vars;
+	}
+    // error_log( 'ordering ' . print_r($vars) );
+	if ( isset( $vars['orderby'] ) && 'scn_course_teacher' == $vars['orderby'] ) {
+		$vars = array_merge( $vars, array(
+				'meta_key' => 'teacher',
+				'orderby' => 'meta_value'
+		) );
+	}
+	elseif ( isset( $vars['orderby'] ) && 'scn_course_tuition' == $vars['orderby'] ) {
+		$vars = array_merge( $vars, array(
+				'meta_key' => 'tuition',
+				'orderby' => 'meta_value_num'
+		) );
+	}
+
+	return $vars;
+}
+
---
+++ class-scn-student.php
@@ -0,0 +1,207 @@
+<?php
+
+/** class */
+
+class SCN_Student {
+    function __construct( $post_id = NULL ) {
+        if ( !empty($post_id)) $this->getPost( $post_id );
+    }
+
+    function getPost( $post_id ) {
+        $this->post = get_post( $post_id );
+
+        // set properties for easy access
+        if ( !empty( $this->post ) ) {
+            $this->id = $this->post->ID;
+            // ...
+        }
+        if ( !empty( $this->id ) ) return $this->id;
+        return false;
+    }
+
+    public static function init() {
+        register_post_type(
+            'scn_student',
+            array(
+                'labels' => array(
+                    'name' => __( 'Students' ),
+                    'singular_name' => __( 'Student' ),
+                    'add_new' => __( 'Add New' ),
+                    'add_new_item' => __( 'Add New Student' ),
+                    'edit_item' => __( 'Edit Student' ),
+                    'new_item' => __( 'New Student' ),
+                    'view_item' => __( 'View Student' ),
+                    'search_item' => __( 'Search Student' ),
+                ),
+                'public' => true,
+                'has_archive' => false,
+                'show_in_menu' => false,
+                'menu_icon' => 'dashicons-book-alt',
+                'supports' => array( 'title', 'comments', 'thumbnail' ),
+				'capability_type' => array('scn_student', 'scn_students'),
+				'map_meta_cap' => true,
+            )
+        );
+    }
+}
+
+add_action( 'init', array('SCN_Student', 'init') );
+
+
+
+// Function to display meta box contents
+function scn_display_student_details_mb( $scn_student ) { 
+	// Retrieve current author and rating based on book review ID
+	$first_name   = get_post_meta( $scn_student->ID, 'first_name', true );
+	$last_name    = get_post_meta( $scn_student->ID, 'last_name', true );
+    $chinese_name = get_post_meta( $scn_student->ID, 'chinese_name', true);
+    $enrollment   = get_post_meta( $scn_student->ID, 'enrollment', true);
+
+	$courses = scn_get_all_courses( '2022 - 2023' );
+	error_log( 'all courses: ' . print_r($courses) );
+
+    ?>
+	<table>
+        <tr>
+			<td style="width: 150px">First Name</td>
+			<td><input type="text" style="width:100%" name="scn_student_first_name" value="<?php echo $first_name; ?>"/></td>
+		</tr>
+		<tr>
+			<td style="width: 150px">Last Name</td>
+			<td><input type="text" style="width:100%" name="scn_student_last_name" value="<?php echo $last_name; ?>" /></td>
+		</tr>
+		<tr>
+			<td style="width: 150px">Chinese Name</td>
+			<td><input type="text" style="width:100%" name="scn_student_chinese_name" value="<?php echo $chinese_name; ?>" /></td>
+		</tr>
+	</table>
+
+<?php }
+
+// Register function to be called when posts are saved
+// The function will receive 2 arguments
+add_action( 'save_post', 'scn_add_student_fields', 10, 2 );
+
+function scn_add_student_fields( $student_id, $student ) {
+    error_log(print_r($student) );
+	if ( 'scn_student' != $student->post_type ) {
+		return;
+	}
+
+	$first_name = ''; // $_POST['scn_student_first_name'];
+	$last_name = ''; $_POST['scn_student_last_name'];
+	
+	if ( isset( $_POST['scn_student_first_name'] ) ) {
+		$first_name = sanitize_text_field( $_POST['scn_student_first_name'] );
+		update_post_meta( $student_id, 'first_name', $first_name );
+	}
+	if ( isset( $_POST['scn_student_last_name'] ) ) 
+	{
+		$last_name = sanitize_text_field( $_POST['scn_student_last_name'] );
+		update_post_meta( $student_id, 'last_name', $last_name );
+	}
+	if ( isset( $_POST['scn_student_chinese_name'] ) ) {
+		update_post_meta( $student_id, 'chinese_name', sanitize_text_field( $_POST['scn_student_chinese_name'] ) );
+	}
+    
+	if (!empty($first_name) || !empty($last_name)) {
+		remove_action( 'save_post', 'scn_add_student_fields' );
+		wp_update_post( array('ID' => $student_id, 'post_title' => $first_name . ' ' . $last_name) );
+		add_action( 'save_post', 'scn_add_student_fields' );
+	}
+	/*******************************************************************
+	* Code from recipe 'Hiding the taxonomy editor from the post editor 
+	* while remaining in the admin menu'
+	*******************************************************************/
+/*
+	if ( isset( $_POST['book_review_book_type'] ) ) {
+		wp_set_post_terms( $book_review->ID, intval( $_POST['book_review_book_type'] ), 'book_reviews_book_type' );
+	}
+*/
+}
+
+/****************************************************************************
+ * Code from recipe 'Displaying additional columns in custom post list page'
+ ****************************************************************************/
+
+// Register function to be called when column list is being prepared
+add_filter( 'manage_edit-scn_student_columns', 'scn_student_add_columns' );
+
+// Function to add columns for author and type in book review listing
+// and remove comments columns
+function scn_student_add_columns( $columns ) {
+    $new_columns = array();
+    $new_columns['scn_student_first_name'] = 'First Name';
+    $new_columns['scn_student_last_name'] = 'Last Name';
+    $new_columns['scn_student_chinese_name'] = 'Chinese Name';
+    
+    unset( $columns['comments'] );
+    $columns = array_slice( $columns, 0, 2 ) + $new_columns + array_slice( $columns, 2 );
+    
+    return $columns;
+}
+
+// Register function to be called when custom post columns are rendered
+add_action( 'manage_posts_custom_column', 'scn_student_populate_columns' );
+
+// Function to send data for custom columns when displaying items
+function scn_student_populate_columns( $column ) {
+	if ( 'scn_student' != get_post_type() ) {
+		return;
+    }
+
+	// Check column name and send back appropriate data
+	if ( 'scn_student_first_name' == $column ) {
+        echo esc_html( get_post_meta( get_the_ID(), 'first_name', true ) );
+    }
+    elseif ( 'scn_student_last_name' == $column ) {
+        echo esc_html( get_post_meta( get_the_ID(), 'last_name', true ) );
+    }
+    elseif ( 'scn_student_chinese_name' == $column ) {
+        echo esc_html( get_post_meta( get_the_ID(), 'chinese_name', true ) );
+    }
+    /*elseif ( 'scn_student_capacity' == $column ) {
+		echo intval( get_post_meta( get_the_ID(), 'capacity', true ) );
+	}
+	elseif ( 'scn_student_tuition' == $column ) {
+		echo '$' . floatval( get_post_meta( get_the_ID(), 'tuition', true ) );
+	}*/
+}
+
+add_filter( 'manage_edit-scn_student_sortable_columns', 'scn_student_column_sortable' );
+
+// Register the author and rating columns are sortable columns
+function scn_student_column_sortable( $columns ) {
+	$columns['scn_student_first_name'] = 'scn_student_first_name';
+	$columns['scn_student_last_name'] = 'scn_student_last_name';
+	$columns['scn_student_chinese_name'] = 'scn_student_chinese_name';
+
+	return $columns;
+}
+
+// Register function to be called when queries are being prepared to
+// display post listing
+add_filter( 'request', 'scn_student_column_ordering' );
+
+// Function to add elements to query variable based on incoming arguments
+function scn_student_column_ordering( $vars ) {
+	if ( !is_admin() ) {
+		return $vars;
+	}
+    // error_log( 'ordering ' . print_r($vars) );
+	if ( isset( $vars['orderby'] ) && 'scn_student_first_name' == $vars['orderby'] ) {
+		$vars = array_merge( $vars, array(
+				'meta_key' => 'first_name',
+				'orderby' => 'meta_value'
+		) );
+	}
+	/*elseif ( isset( $vars['orderby'] ) && 'scn_student_tuition' == $vars['orderby'] ) {
+		$vars = array_merge( $vars, array(
+				'meta_key' => 'tuition',
+				'orderby' => 'meta_value_num'
+		) );
+	}*/
+
+	return $vars;
+}
+
